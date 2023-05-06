516d6a25769ed1b0b58b6e2ff1d06a19bf1ad778
Vitor Boccio <vitorboccio@gmail.com>
2023-05-06 15:21:54 -0700

    listing jobs

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -1,41 +1,41 @@
 import { Button } from '@mui/material'
+import { DataGrid, GridRenderCellParams, GridValueGetterParams } from '@mui/x-data-grid'
+import { useQuery } from '@tanstack/react-query'
+import { fetchApi } from 'client'
 import Link from 'next/link'
-const Jobs = () => {
-  // const createJob = async () => {
-  //   try {
-  //     await fetchApi({
-  //       url: '/job',
-  //       body: {
-  //         id: Math.ceil(Math.random() * 10000),
-  //         title: 'Front-End Senior Test Engineer',
-  //         applicants: [],
-  //         locationType: 'Remote',
-  //         description: `description fake num ${Math.ceil(Math.random() * 100000)}`,
-  //         location: {
-  //           country: 'United States',
-  //           city: 'New York',
-  //           province: 'New York',
-  //         },
-  //         tags: ['react', 'javascript', 'typescript', 'html', 'css', 'jest', 'cypress'],
-  //         salary: {
-  //           min: 100000,
-  //           max: 120000,
-  //           period: 'annual',
-  //           currency: 'USD',
-  //         },
-  //       },
-  //     })
-  //   } catch (error) {
-  //     console.error({ error })
-  //   }
-  // }
 
+const Jobs = () => {
+  const jobsQuery = useQuery({
+    queryKey: ['jobs'],
+    queryFn: async () => await fetchApi({ url: '/job', method: 'GET' }),
+  })
+  console.log({ jobsQuery })
+  
+  if (jobsQuery.isLoading) return <div>Loading...</div>
+  if (jobsQuery.isError) return <div>Error...</div>
   return (
     <div>
       <h1>Jobs</h1>
       <Link href="/jobs/create">
         <Button variant="contained">Create Job</Button>
       </Link>
+      <DataGrid 
+        rows={jobsQuery.data || []}
+        columns={[
+          { field: 'title', headerName: 'Title', flex: 1 },
+          { field: 'description', headerName: 'Description', flex: 1 },
+          { field: 'locationType', headerName: 'Location Type', flex: 1 },
+          { field: 'location.country', headerName: 'Country', flex: 1, 
+            renderCell: (params: GridRenderCellParams) => <strong>{params.row.location.country}</strong> },
+          { field: 'applicants', headerName: 'Applicants', 
+            valueGetter: (params: GridValueGetterParams) => params.row.applicants.length,
+            flex: 1,
+          },
+        ]}
+        autoHeight
+        pageSizeOptions={[5, 10, 20]}
+        
+      />
     </div>
   )
 }
